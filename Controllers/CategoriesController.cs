4af5aef23e9f18ece9ef1e6f0b4898dd981d4ee1
qWojtpl <71585628+qWojtpl@users.noreply.github.com>
2023-07-06 17:53:08 +0200

    Edit category
    - Now able to edit category

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -41,7 +41,7 @@ namespace Sklepix.Controllers
         // POST: CategoriesController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind("ID,Name,Description")] Category category)
+        public ActionResult Create([Bind("Name,Description")] Category category)
         {
             if(!isCategoryCorrect(category))
             {
@@ -62,38 +62,66 @@ namespace Sklepix.Controllers
         // GET: CategoriesController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            Category? category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return RedirectToAction("Index", "Categories");
+            }
+            return View(category);
         }
 
         // POST: CategoriesController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit([Bind("ID,Name,Description")] Category category)
         {
+            if(!isCategoryCorrect(category))
+            {
+                return View(category);
+            }
             try
             {
-                return RedirectToAction(nameof(Index));
+                Category? oldCategory = _context.Categories.Find(category.ID);
+                if(oldCategory == null)
+                {
+                    return View(category);
+                }
+                _context.Categories.Remove(oldCategory);
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Categories");
             }
             catch
             {
-                return View();
+                return View(category);
             }
         }
 
         // GET: CategoriesController/Delete/5
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id)
         {
-            return View();
+            Category? category = _context.Categories.Find(id);
+            if(category == null)
+            {
+                return RedirectToAction("Index", "Categories");
+            }
+            return View(category);
         }
 
         // POST: CategoriesController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int id, IFormCollection collection)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                Category? category = _context.Categories.Find(id);
+                if(category != null)
+                {
+                    _context.Categories.Remove(category);
+                    _context.SaveChanges();
+                }
+                return RedirectToAction("Index", "Categories");
             }
             catch
             {
@@ -105,16 +133,19 @@ namespace Sklepix.Controllers
         {
             if("".Equals(category.Name) || category.Name == null)
             {
+                ModelState.AddModelError("Name", "This field is required");
                 return false;
             }
             if(category.Name.Contains("<") || category.Name.Contains(">"))
             {
+                ModelState.AddModelError("Name", "This field can't contain tag symbols");
                 return false;
             }
             if(category.Description != null)
             {
                 if(category.Description.Contains("<") || category.Description.Contains(">"))
                 {
+                    ModelState.AddModelError("Name", "This field can't contain tag symbols");
                     return false;
                 }
             }
